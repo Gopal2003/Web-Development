682b1646b1e618940ba9a24d755e91a862afcf06
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-06-03 20:01:50 +0530

    Basic JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,4 @@
+
 const obj = {
     name : "Gopal",
     Age : 12,
@@ -8,13 +9,13 @@ const obj = {
     // key: "Value" ! explanation is in notes. First try to recall it yourself.
 
 };
-
+/*
 for(var key in obj)
 {
     console.log(key,obj.key);
 }    
 // console.log(obj.name);
-/*
+
 // obj.printHello();
 
 obj.name = "Agarwal";
@@ -31,5 +32,33 @@ console.log(obj["name"]);
 console.log(obj.name);
 // console.log(obj[name]); deprecated.
 */
+
+function abcd( a)
+{
+    // a();
+    console.log(a);
+}
+
+// abcd(function(){console.log("Heelllo");}); // first classs function..
+// var a = 12;
+
+// abcd(a);
+
+var a =[1.12,2,"gopal",4,5];
+console.log(typeof(a));
+console.log(Array.isArray(a))
+console.log(Array.isArray(obj))
+
+
+a[-1] = 12;
+console.log(a)
+
+delete obj.name;
+
+
+
+
+
+
 
 
