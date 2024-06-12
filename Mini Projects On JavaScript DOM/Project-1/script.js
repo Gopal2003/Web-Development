a9acadfbf35b4d9e6f683fe0694461966e09a09e
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-06-12 19:50:21 +0530

    Start of Project-1 of DOM

--- Mini Projects On JavaScript DOM/Project-1/script.js
+++ Mini Projects On JavaScript DOM/Project-1/script.js
@@ -1,1 +1,19 @@
+//Checking 
 console.log("hello");
+
+//Retriving the necessary HTML elements using their IDs
+var adBtn = document.querySelector("#add");
+var stat = document.querySelector("#status");
+var rmvBtn = document.querySelector("#remove");
+
+
+
+//Adding the Event Listener to the Add Friend Button
+adBtn.addEventListener("click",function(){
+    stat.textContent = "Friend";
+})
+
+//Adding the Event Listener to the Remove Button
+rmvBtn.addEventListener("click",()=>{
+    stat.textContent = "Stranger";
+})
